47a4b58ee065dc8a22bd5b47efc5d86f7eccce0e
Jatin_1308 <jatinbatish098@gmail.com>
2024-06-20 01:37:24 +0530

    weighteduniformstrings

--- WeightedUniformStrings.py
+++ WeightedUniformStrings.py
@@ -1,13 +1,51 @@
-def weightedStrings(s):
-    d ={}
-    for char in s:
-        if char not in d:
-            d[char] = 1
-        else:
-            d[char]+=1
+def weightedUniformStrings(s,queries):
+    if len(s)==0:
+        return ["No"]*len(queries)
     
-    for val in d.values:
+
+    i,j=0,1
+    alphabet_dict = {}
+    for i in range(26):
+        alphabet_dict[chr(ord('a') + i)] = i + 1
+    print(alphabet_dict)
+
+    arr = []
+    i = 0
+    j = 1
+    freqDict = {}
+    if len(s) == 1:
+            freqDict[alphabet_dict[s]] = True
+    while i<len(s) and j<len(s):
+        if i == 0:
+            freqDict[alphabet_dict[s[i]]] = True
         
+        if s[i]!=s[j]:
+            if s[j] not in freqDict:
+                freqDict[alphabet_dict[s[j]]] = True
+            i = j
+            j+=1
+        else:
+            if s[i:j+1] not in freqDict:
+                freqDict[alphabet_dict[s[i:j+1][0]]*len(s[i:j+1])] = True
+            j+=1
+    
+    print(freqDict)
+
+    for i in range(len(queries)):
+        if queries[i] in freqDict:
+            queries[i] = 'Yes'
+        else:
+            queries[i] = 'No'
+
+    return queries
 
 
-weightedStrings('abbcccdddd')
+
+# print(weightedUniformStrings("",[1,7,5,4,15]))
+
+# print(weightedUniformStrings("aaaaaa",[1,4,7,3]))
+
+print(weightedUniformStrings('l',[1,12]))
+
+# print(weightedUniformStrings('abbcccdddd',[1,7,5,4,15]))
+# print(weightedStrings('abcdeabcd',[1,7,5,4,15]))
